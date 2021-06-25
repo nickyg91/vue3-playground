d9c7fa79b05e610177b5ea9be62a06c500aab68a
Nick Ganter <nickganter91@gmail.com>
2021-06-25 16:26:23 -0500

    beginnings of playground

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,55 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <Menubar class="p-menubar" :model="routes"></Menubar>
+  <div class="content">
+    <router-view />
   </div>
-  <router-view />
 </template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import Menubar from "primevue/menubar";
+export default defineComponent({
+  components: {
+    Menubar,
+  },
+  setup() {
+    const routes = ref([
+      {
+        label: "Home",
+        to: "/",
+      },
+      {
+        label: "About",
+        to: "/About",
+      },
+    ]);
+    return {
+      routes,
+    };
+  },
+});
+</script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "~primevue/resources/themes/vela-blue/theme.css"; //theme
+@import "~primevue/resources/primevue.min.css"; //core css
+@import "~primeicons/primeicons.css"; //icons
+@import "~primeflex/primeflex.css";
+html {
+  min-height: 100vh;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.content {
+  padding-left: 10%;
+  padding-right: 10%;
+  justify-content: center;
+  display: flex;
+}
+.p-menubar {
+  border-top-left-radius: 0px;
+  border-top-right-radius: 0px;
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px;
+}
+body {
+  margin: 0;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
-createApp(App).use(store).use(router).mount("#app");
+import PrimeVue from "primevue/config";
+createApp(App).use(store).use(router).use(PrimeVue, { ripple: true }).mount("#app");
